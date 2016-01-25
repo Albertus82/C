68da28ffd59211292d05a0b07ac5ef84510584cb
Albertus82 <albertus82@gmail.com>
2016-01-25 22:01:18 +0100

    s

--- Date/date.c
+++ Date/date.c
@@ -91,8 +91,3 @@ int main(void) {
 	printf("Fine.\n");
 	return 0;
 }
-<<<<<<< HEAD
-// ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ
-=======
->>>>>>> branch 'master' of https://github.com/Albertus82/C.git
-// AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
